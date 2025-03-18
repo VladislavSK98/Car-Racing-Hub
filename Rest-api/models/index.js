a54434cc786b4522a4a742d9700bed14ed7c3d5e
VladislavSK98 <Vladislavkotubov@gmail.com>
2025-03-18 19:28:06 +0200

    Initial commit - Vite project setup

---
+++ Rest-api/models/index.js
@@ -0,0 +1,13 @@
+const userModel = require('./userModel');
+const tokenBlacklistModel = require('./tokenBlacklistModel');
+const themeModel = require('./themeModel');
+const postModel = require('./postModel');
+const carModel = require('./carModel');
+
+module.exports = {
+    userModel,
+    tokenBlacklistModel,
+    themeModel,
+    postModel,
+    carModel,
+}
