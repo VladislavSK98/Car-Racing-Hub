99e28bdbc7a57b185afd08acc8592014013c5842
VladislavSK98 <vladislavkotubov@gmail.com>
2025-04-07 22:05:15 +0300

    Delete car-racing-hub directory

--- car-racing-hub/src/components/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import styles from '../Footer/Footer.module.css';
-
-const Footer = () => {
-    return <footer className={styles.footer}>© 2025 Car Racing Hub. All Rights Reserved.</footer>;
-};
-
-export default Footer;
--- car-racing-hub/src/pages/Details.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router-dom";
-
-function Details() {
-  const { id } = useParams();
-
-  return <h1>Details Page for Item {id}</h1>;
-}
-
-export default Details;
